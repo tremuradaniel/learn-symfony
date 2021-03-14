e995b9b43eaba7d5cb0c4b5ad9ce3094f9603e87
Daniel Tremura <tremuradaniel@gmail.com>
2021-03-14 14:44:44 +0200

    Doctrine one-to-many & many-to-one relationships

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Entity\Video;
 use App\Services\GiftsService;
 use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\Request;
@@ -272,6 +273,53 @@ class DefaultController extends AbstractController
         $user->setName('Robert');
         $entityManager->persist($user);
         $entityManager->flush();
+
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController'
+        ]);
+    }  
+
+        /**
+     * @Route("/video")
+     */
+    public function video()
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        // $user = new User();
+
+        // $user->setName('Robert');
+        // for ($i=0; $i < 4; $i++) { 
+        //     $video = new Video();
+        //     $video->setTitle('Video title - ' . $i);
+        //     $user->addVideo($video);
+        //     $entityManager->persist($video);
+             
+        // }
+        // $entityManager->persist($user);
+        // $entityManager->flush();
+
+        // dump('Created a video with the id of ' . $video->getId());
+        // dump('Created an user with the id of ' . $user->getId());
+
+
+        $video = $this->getDoctrine()
+            ->getRepository(Video::class)
+            ->find(1);
+
+        dump('Get user info of video ');
+
+        // this return some fields as null because symfony save resource
+        dump($video->getUser());
+
+        dump($video->getUser()->getName());
+
+        $user = $this->getDoctrine()
+            ->getRepository(User::class)
+            ->find(1);
+
+        foreach ($user->getVideos() as $video) {
+            dump($video->getTitle());
+        }
 
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController'
