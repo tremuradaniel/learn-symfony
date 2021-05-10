f25d1c1ceaa958f3ba47aea63de15aedf1a157f5
Daniel Tremura <tremuradaniel@gmail.com>
2021-05-10 09:54:47 +0300

    Functional tests - send a form

--- tests/DefaultControllerTest.php
+++ tests/DefaultControllerTest.php
@@ -9,17 +9,26 @@ class DefaultControllerTest extends WebTestCase
     public function testSomething()
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/');
+        $crawler = $client->request('GET', '/login');
 
-        $this->assertSame(200, $client->getResponse()->getStatusCode());
-        $this->assertStringContainsString('Hello', $crawler->filter('h1')->text());
+        $form = $crawler->selectButton('Sign in')->form();
+        $form['email'] = 'user@user.com';
+        $form['password'] = 'passw';
 
-        $link = $crawler
-            ->filter('a:contains("awesome link")')
-            ->link();
+        $crawler = $client->submit($form);
+        $crawler = $client->followRedirect();
 
-        $crawler = $client->click($link);
+        $this->assertEquals(1, $crawler->filter('a:contains("logout")')->count());
 
-        $this->assertStringContainsString('Remember me', $client->getResponse()->getContent());
+        // $this->assertSame(200, $client->getResponse()->getStatusCode());
+        // $this->assertContains('Hello', $crawler->filter('h1')->text());
+
+        // $link = $crawler
+        // ->filter('a:contains("awesome link")')
+        // ->link();
+
+        // $crawler = $client->click($link);
+        // $this->assertContains('Remember me', $client->getResponse()->getContent());
+
     }
 }
