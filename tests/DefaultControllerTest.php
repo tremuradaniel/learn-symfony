48f4d1f517d31e82c2b81a987a42b952e0d370f6
Daniel Tremura <tremuradaniel@gmail.com>
2021-05-10 09:57:03 +0300

    Functional tests - data providers

--- tests/DefaultControllerTest.php
+++ tests/DefaultControllerTest.php
@@ -6,29 +6,23 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testSomething()
+    /**
+     * @dataProvider provideUrls
+     */
+    public function testSomething($url)
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/login');
+        $crawler = $client->request('GET', $url);
 
-        $form = $crawler->selectButton('Sign in')->form();
-        $form['email'] = 'user@user.com';
-        $form['password'] = 'passw';
+        $this->assertTrue($client->getResponse()->isSuccessful());
 
-        $crawler = $client->submit($form);
-        $crawler = $client->followRedirect();
+    }
 
-        $this->assertEquals(1, $crawler->filter('a:contains("logout")')->count());
-
-        // $this->assertSame(200, $client->getResponse()->getStatusCode());
-        // $this->assertContains('Hello', $crawler->filter('h1')->text());
-
-        // $link = $crawler
-        // ->filter('a:contains("awesome link")')
-        // ->link();
-
-        // $crawler = $client->click($link);
-        // $this->assertContains('Remember me', $client->getResponse()->getContent());
-
+    public function provideUrls()
+    {
+        return [
+            ['/'],
+            ['/login']
+        ];
     }
 }
